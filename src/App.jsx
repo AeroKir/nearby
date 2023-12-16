608a155b73457821f863568796ee301209c03d89
Kirill_Shevtsov <shev.kv.1982@gmail.com>
2023-12-16 23:21:45 +0200

    fetch request added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [repairers, setRepairers] = useState([]);
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:16001/repairers')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setRepairers(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch('http://localhost:16001/customers')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setCustomers(data);
+      });
+  }, []);
 
   return (
     <>
@@ -28,6 +52,29 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <div>
+
+      <h1>This is repairs</h1>
+      {repairers.map((repairer) => (
+        <>
+          <ul key={repairer.id}>
+            <li>{repairer.name}</li>
+          </ul>
+        </>
+
+      ))}
+
+      <h1>This is customers from another microservice</h1>
+      {customers.map((customer) => (
+        <>
+          <ul key={customer.id}>
+            <li>{customer.name}</li>
+          </ul>
+        </>
+
+      ))}
+    </div>
     </>
   )
 }
